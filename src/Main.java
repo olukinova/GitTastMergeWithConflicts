6c4bcb5afc6a5512e32e88ca830bc5ce95f5729f
Olena Lukinova <95899106+olukinova@users.noreply.github.com>
2023-04-10 22:33:50 -0500

    2nd commit made by member A

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("this code was added by member A to the line 4");
+        System.out.println("this code was added by member A to the line 4 to the corresponding branch");
     }
 }
