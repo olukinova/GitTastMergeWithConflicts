3810da4a30f94291654326b62626a6e4f2ed9fb0
Olena Lukinova <95899106+olukinova@users.noreply.github.com>
2023-04-10 22:32:29 -0500

    1st commit made by member A

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("this code was added by member A to the line 4");
     }
 }
